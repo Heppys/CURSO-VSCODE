9719846bd537755169f6eeecdd6b50d10ad4bf21
Heppys <fal.heppys@gmail.com>
2023-08-02 20:40:12 -0300

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+function HelloWorld()
+{
+console.log("Hello World!!!");
+}
+
+const saudacao = ()=>
+{
+    var data = new Date()
+return data.getHours() <= 12? "Bom dia!!!": data.getHours() <= 18? "Boa tarde!!!": "Boa noite!!!"
+}    
+
+//testetetetststetet => 
+HelloWorld();
+console.log('A saudacao do momento é ' + saudacao());
